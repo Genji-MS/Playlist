7570e6f8d7f4f0c19cb240e633377fce921c271d
Genji-MS <Genji.Tapia@students.makeschool.com>
2019-09-28 12:33:06 -0700

    text array of playlist info visible

--- app.py
+++ app.py
@@ -2,10 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+playlist = [
+    {'title': 'Twitch Fails', 'description': 'Live streaming video-game funny moments'},
+    {'title': 'New Retro Music', 'description': 'Technology, Metal, Chiptune fusion'}
+]
+
+#@app.route('/')
 def index():
     """Return homepage"""
     return render_template('home.html', msg='flask flask baby~')
+
+@app.route('/')
+def playlist_index():
+    """Show All playlists"""
+    return render_template('playlist_index.html', playlist= playlist)
 
 if app.name == '__main__':
     app.run(debug=True)
