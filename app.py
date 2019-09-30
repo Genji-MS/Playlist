b0e27acf8ce5141bdd5da558edddb828924c3624
Genji-MS <Genji.Tapia@students.makeschool.com>
2019-09-30 16:52:47 -0700

    modified with copyright bootstrap credits, timestamps, and a different theme. This is the local server version, heroku code is commented out

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+from datetime import datetime
 
 #heroku version
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlist')
@@ -45,7 +46,8 @@ def playlist_submit():
     p_list= { 
         'title': request.form.get('title'),
         'description': request.form.get('description'),
-        'videos': request.form.get('videos').split()
+        'videos': request.form.get('videos').split(),
+        'created_at': datetime.now()
     }
     #Youtube doesn't like to display in this way, replace this component of the URL to embed/ and it'll work fine.
     for index in range(len(p_list['videos'])):
@@ -107,6 +109,13 @@ def comments_new():
     comment_id = comments.insert_one(comment).inserted_id
     return redirect(url_for('playlist_show', playlist_id=request.form.get('playlist_id')))
     #return 'playlist comments'
+
+@app.route('/playlist/comments/<comment_id>', methods=['POST'])
+def comments_delete(comment_id):
+    """Action to delete a comment"""
+    comment = comments.find_one({'_id': ObjectId(comment_id)})
+    comments.delete_one({'_id': ObjectId(comment_id)})
+    return redirect(url_for('playlist_show', playlist_id=comment.get('playlist_id')))
 
 if app.name == '__main__':
     #app.run(debug=True)
