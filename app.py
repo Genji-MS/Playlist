e16a3e7304472e0add007a194ae66ffb7d7d324b
Genji-MS <Genji.Tapia@students.makeschool.com>
2019-09-29 19:00:11 -0700

    changed False to false for rewrites

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlist')
-client = MongoClient(host=f'{host}?retryWrites=False')
+client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 playlist= db.playlist
 
